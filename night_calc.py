1c02f4816d548b882fb0291077b6b3f1ea2f6f44
Eduardo Alberto Duarte Lacerda <dhubax@gmail.com>
2023-07-17 12:51:57 -0300

    update Observer

--- night_calc.py
+++ night_calc.py
@@ -1,12 +1,10 @@
 import sys
+import pytz
 import warnings
 import argparse as ap
 import astropy.units as u
 from os.path import basename
-from zoneinfo import ZoneInfo
-from astropy.time import Time
-from astroplan import Observer
-from datetime import datetime, timezone
+from datetime import datetime
 from astropy.coordinates import EarthLocation, Angle
 
 warnings.filterwarnings('ignore')
@@ -33,9 +31,9 @@ def parse_arguments():
         if '-' not in args.date:
             parser.print_help()
             sys.exit(1)
-        args.date += ' 23:59:59.000+00:00'
+        args.isoformatdt = args.date + ' 23:59:59'
         try:
-            args.dt_obs = datetime.fromisoformat(args.date)
+            args.dt_obs = datetime.fromisoformat(args.isoformatdt)
         except ValueError as e:
             print(f'{__script_name__}: {e}\n')
             parser.print_help()
@@ -43,43 +41,42 @@ def parse_arguments():
 
     return args
 
+def create_T80S_Observer():
+    from astroplan import Observer
+    
+    # T80-South location information
+    return Observer(
+        location=EarthLocation(
+            lat=Angle('-30d10m04.31s', 'deg'), 
+            lon=Angle('-70d48m20.48s', 'deg'), 
+            height=2178*u.m,
+        ), 
+        timezone=pytz.timezone('America/Santiago'), 
+        name='T80-South',
+    )
+
 if __name__ == '__main__':
     args = parse_arguments()
 
-    # T80-South location information
-    T80S_TZ = ZoneInfo('America/Santiago')
-    UTC_TZ = timezone.utc
-    T80S_LAT = '-30d10m04.31s'
-    T80S_LON = '-70d48m20.48s'
-    T80S_HEI = 2178
-    t80s_lat = Angle(T80S_LAT, 'deg')
-    t80s_lon = Angle(T80S_LON, 'deg')
-    t80s_hei = T80S_HEI*u.m
-    t80s_EL = EarthLocation(lat=t80s_lat, lon=t80s_lon, height=t80s_hei)
+    t80s_obs = create_T80S_Observer()
+    local_Time = t80s_obs.datetime_to_astropy_time(args.dt_obs)
 
-    # Date and time configuration   
-    t80s_dt = args.dt_obs.astimezone(T80S_TZ)
-    t80s_Time = Time(t80s_dt - t80s_dt.utcoffset(), location=t80s_EL)
-
-    # Observer at T80-South location
-    t80s_obs = Observer(location=t80s_EL, timezone=T80S_TZ)
-    
     # Nearest evening and morning twilights timestamp and LST 
-    twilight_evening = t80s_obs.twilight_evening_astronomical(t80s_Time, which='nearest')
-    twilight_morning = t80s_obs.twilight_morning_astronomical(t80s_Time, which='nearest')
+    twilight_evening = t80s_obs.twilight_evening_astronomical(local_Time, which='nearest')
+    twilight_morning = t80s_obs.twilight_morning_astronomical(local_Time, which='nearest')
     lst_even = t80s_obs.local_sidereal_time(twilight_evening)
     lst_morn = t80s_obs.local_sidereal_time(twilight_morning)
     
     # Night duration (time between twilights)
-    night_duration = (twilight_morning.datetime - twilight_evening.datetime).total_seconds()/3600
+    night_duration = (twilight_morning - twilight_evening).to_value('hr')
 
     # Moon at midnight
     if args.local_midnight:
-        Time_mn = t80s_Time
+        local_Time_mn = local_Time
     else:
-        Time_mn = t80s_obs.midnight(t80s_Time)
-    moon_illum = t80s_obs.moon_illumination(Time_mn)
-    lst_mn = t80s_obs.local_sidereal_time(Time_mn)    
+        local_Time_mn = t80s_obs.midnight(local_Time)
+    moon_illum = t80s_obs.moon_illumination(local_Time_mn)
+    lst_mn = t80s_obs.local_sidereal_time(local_Time_mn)    
 
     # Make relatory
     if args.csv:
@@ -90,7 +87,7 @@ if __name__ == '__main__':
                 lst_even.to_string(), 
                 twilight_morning.datetime, 
                 lst_morn.to_string(), 
-                Time_mn.datetime,
+                local_Time_mn.datetime,
                 lst_mn.to_string(),
                 moon_illum,
             )
@@ -99,14 +96,17 @@ if __name__ == '__main__':
         print(f'Night duration: {night_duration:.2f} hours')
         print(f'Evening twilight:')
         print(f'\tDATETIME (UTC):', twilight_evening.datetime)
+        print(f'\tDATETIME (LOCAL):', t80s_obs.astropy_time_to_datetime(twilight_evening))
         print(f'\tLST:', lst_even)
         print(f'Morning twilight:')
         print(f'\tDATETIME (UTC):', twilight_morning.datetime)
+        print(f'\tDATETIME (LOCAL):', t80s_obs.astropy_time_to_datetime(twilight_morning))
         print(f'\tLST:', lst_morn)
         if args.local_midnight:
             print('Using local midnight:')
         else:
             print(f'Sun midnight:')
-            print(f'\tDATETIME (UTC):', Time_mn.datetime)
+            print(f'\tDATETIME (UTC):', local_Time_mn.datetime)
+            print(f'\tDATETIME (LOCAL):', t80s_obs.astropy_time_to_datetime(local_Time_mn))
             print(f'\tLST:', lst_mn)
         print(f'\tMoon illumination: {moon_illum:.4f}')
